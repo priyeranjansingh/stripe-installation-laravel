515ab39a15ea80e9765a0be8402c3746dd7a3763
priyaranjan singh <58334735+priyeranjansingh@users.noreply.github.com>
2021-10-19 18:55:27 +0530

    Create page.php

---
+++ page.php
@@ -0,0 +1,3 @@
+1. composer create-project --prefer-dist laravel/laravel blog to install --laravel project
+2. composer install --to install composer
+3. composer require stripe/stripe-php --to install stripe library
