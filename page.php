637bc4a0f8e646ee192049bff9f3377ff2c4fe93
priyaranjan singh <58334735+priyeranjansingh@users.noreply.github.com>
2021-10-19 18:59:59 +0530

    Update page.php

--- page.php
+++ page.php
@@ -1,3 +1,10 @@
 1. composer create-project --prefer-dist laravel/laravel blog to install --laravel project
 2. composer install --to install composer
 3. composer require stripe/stripe-php --to install stripe library
+4. paste stripePaymentController.php page code to your controller
+5. paste stripeviewpage.php code to your view page
+5. .env file add 
+STRIPE_KEY=pk_test_******************************
+STRIPE_SECRET=sk_test_*******************************
+
+And now run the application to test the code
